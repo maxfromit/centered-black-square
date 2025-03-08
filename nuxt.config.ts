28fbbcbf5a3ea9c144ffbdb5ef1d4d7e220fe4cf
maximfromit <bigmaxcapacity@gmail.com>
2025-03-08 18:26:57 +0300

    nuxt.config: format code and add nitro prerender configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/eslint'
-  ],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
 
-  compatibilityDate: '2024-11-27'
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  },
+
+  compatibilityDate: "2024-11-27",
 })
